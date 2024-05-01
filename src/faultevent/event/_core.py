2cf17c05f2707d43206bd82f40ce40972f5b0fd8
Arild Bergesen Husebø <arhusebo@gmail.com>
2024-05-01 12:42:57 +0200

    add cumulative event spectrum and new event selection method

--- src/faultevent/event/_core.py
+++ src/faultevent/event/_core.py
@@ -1,13 +1,21 @@
 import numpy as np
 import numpy.typing as npt
 
+
+def fourier_sequence(
+        f: npt.ArrayLike,
+        x: npt.ArrayLike,
+        w: npt.ArrayLike) -> np.ndarray:
+    return w*np.exp(-2j*np.pi*np.outer(f, x))
+
+
 def weighted_event_spectrum(
         f: npt.ArrayLike,
         x: npt.ArrayLike,
         w: npt.ArrayLike) -> np.ndarray:
     """Evaluates the weighted event spectrum at f given locations x and
     weights w"""
-    return np.sum(w*np.exp(-2j*np.pi*np.outer(f, x)), axis=-1)
+    return np.sum(fourier_sequence(f, x, w), axis=-1)
 
 
 def event_spectrum(
@@ -15,6 +23,13 @@ def event_spectrum(
         x: npt.ArrayLike) -> np.ndarray:
     """Evaluates the event spectrum at f given locations x """
     return weighted_event_spectrum(f, x, 1.0)
+
+
+def cumulative_event_spectrum(
+        f: npt.ArrayLike,
+        x: npt.ArrayLike) -> np.ndarray:
+    """Evaluates the event spectrum at f given locations x """
+    return np.cumsum(fourier_sequence(f, x, 1.0))
 
 
 def map_circle(f: float, x: npt.ArrayLike) -> np.ndarray:
--- src/faultevent/event/_util.py
+++ src/faultevent/event/_util.py
@@ -1,7 +1,7 @@
 import numpy as np
 import numpy.typing as npt
 
-from . import event_spectrum
+from . import event_spectrum, cumulative_event_spectrum
 
 def find_order(x: npt.ArrayLike, ordmin: float, ordmax: float,
                density: float = 10000.0) -> tuple[float, float]:
@@ -24,3 +24,19 @@ def fit_vonmises(f: float, x: npt.ArrayLike) -> tuple[float, float]:
     mu = np.angle(r)
     kappa = (1.28-0.53*ar**2)*np.tan(np.pi/2*ar)
     return mu, kappa
+
+def best_events(
+        y: npt.ArrayLike,
+        x: npt.ArrayLike,
+        f: tuple[float]
+    ):
+    """Returns the indices of the "best" subset of events by evaluating
+    an inclusion criterion against an ordered cummulative sum of
+    event spectra, evaluated at a specified frequency."""
+    idx_sort = np.argsort(y)[::-1]
+    _y = y[idx_sort]
+    _x = x[idx_sort]
+    ces = cumulative_event_spectrum(f, _x)
+    crit = np.abs(ces)/np.sqrt(np.arange(len(ces))+1)
+    idx_best = np.argmax(crit)
+    return _y[:idx_best], _x[:idx_best]
